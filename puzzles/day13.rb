ff64cc030fe56ce9748d6394f7d727d62bdb2fb7
Jason Knoll <jason.p.knoll@gmail.com>
2021-12-15 22:45:15 -0700

    Add solution for day 13 part b

--- puzzles/day13.rb
+++ puzzles/day13.rb
@@ -45,6 +45,31 @@ def plot_paper
   all_spots = paper.select {|key, value| value == true}
   puts "After one fold there are #{first_pass} visible dots."
   puts "After all #{fold_count} folds, there are #{all_spots.count} visible dots."
+  print_board(all_spots)
+end
+
+def print_board(all_spots)
+  length = all_spots.keys.map {|key| key.to_s.split("_")[0].to_i}.max
+  width = all_spots.keys.map {|key| key.to_s.split("_")[1].to_i}.max
+  board = {}
+  (0..length).each do |length|
+    (0..width).each do |width|
+      if all_spots[:"#{length}_#{width}"]
+        board[:"#{length}_#{width}"] = "#"
+      else
+        board[:"#{length}_#{width}"] = "."
+      end
+    end
+  end
+  current_width = 0
+  (width+1).times do
+    layer = board.select do |key, value|
+      key.to_s.split("_")[1].to_i == current_width
+    end
+    p layer.values.join(", ").gsub(",", "")
+    current_width += 1
+  end
+  puts
 end
 
 puts plot_paper
