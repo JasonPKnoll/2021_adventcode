d8a24c65b54a38ea6c1daf86ea2f76b0cd2f601f
Jason Knoll <jason.p.knoll@gmail.com>
2021-12-22 02:37:02 -0700

    Add solution for day 15 part b

--- puzzles/day15.rb
+++ puzzles/day15.rb
@@ -20,6 +20,7 @@ def get_data
         add += 1
       end
       row += 1
+
       column = 0
       add = 0
     end
@@ -30,27 +31,39 @@ def get_data
 end
 
 $data = get_data
+$iterations = 0
+$change = nil
 
 def find_path
-  find_lowest_risk_route($data)
+  until $change == 0
+    $change = 0
+    weight_nodes($data)
+    $iterations += 1
+    puts "iteration: #{$iterations}, total changes: #{$change}"
+  end
   risk = $data.to_a.last[1][1] - $data.to_a.first[1][1]
   puts "Lowest total risk is #{risk}"
 end
 
-def find_lowest_risk_route(grid)
+def weight_nodes(grid)
   grid.map do |position|
     nearby = get_nearest_spots(position, grid)
     nearby = nearby.select {|neighbor| neighbor[1][1] != nil}
     nearby = [[:"nil", ["0", 0]]] if nearby.empty?
-    position[1][1] = position[1][0].to_i + nearby.map {|key, value| value[1]}.min
+    if position[1][1] == nil || position[1][1] > nearby.map {|key, value| value[1]}.min + position[1][0]
+      $change += 1
+      position[1][1] = position[1][0].to_i + nearby.map {|key, value| value[1]}.min
+    end
   end
 end
 
 def get_nearest_spots(spot, grid)
   paths = []
   key = spot[0].to_s.split("_").map {|number| number.to_i}
+  paths << grid.assoc(:"#{key[0]}_#{key[1]+1}")
   paths << grid.assoc(:"#{key[0]}_#{key[1]-1}")
   paths << grid.assoc(:"#{key[0]-1}_#{key[1]}")
+  paths << grid.assoc(:"#{key[0]+1}_#{key[1]}")
   paths.compact
 end
 
